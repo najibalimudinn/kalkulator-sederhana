54dc9db61fcfba094ae67ed7a719381673097f92
najibalimudinn <najibaf147@gmail.com>
2025-04-25 14:58:48 +0700

    Minor update

--- src/main/java/com/najib/Main.java
+++ src/main/java/com/najib/Main.java
@@ -4,7 +4,10 @@ import com.najib.modules.Controller;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Kalkulator Sederhana");
+        System.out.println("==========================\n");
         int hasil = Controller.runKalkulator();
+        System.out.println("\n==========================");
         System.out.println("Hasil: " + hasil);
     }
 }
