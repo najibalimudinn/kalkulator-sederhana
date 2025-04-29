7249dcbc8e0a9cbccb7a80657f2579f42a23da2a
najibalimudinn <najibaf147@gmail.com>
2025-04-29 07:48:32 +0700

    Minor update

--- src/main/java/com/najib/modules/Operation.java
+++ src/main/java/com/najib/modules/Operation.java
@@ -2,13 +2,21 @@ package com.najib.modules;
 
 public class Operation {
     public static int calculate(int a, int b, char operation) {
-        int hasil = switch (operation) {
-            case '+' -> a + b;
-            case '-' -> a - b;
-            case '*' -> a * b;
-            case '/' -> a / b;
-            default -> 0;
-        };
+        int hasil = 0;
+        switch (operation) {
+            case '+':
+                hasil = a + b;
+                break;
+            case '-':
+                hasil = a - b;
+                break;
+            case '*':
+                hasil = a * b;
+                break;
+            case '/':
+                hasil = a / b;
+                break;
+        }
         return hasil;
     }
 }
